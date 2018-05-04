3248dd569e14d1cb67839f634f65f51e22d844a7
Jeph Kryzak <jkryzak@credera.com>
2018-05-03 21:10:00 -0500

    output conditions

--- src/App.js
+++ src/App.js
@@ -10,15 +10,43 @@ const API_KEY = "dj0yJmk9NXJQVDI1d2thUFlOJmQ9WVdrOVRHcHpRemxXTm5VbWNHbzlNQS0tJnM
 
 class App extends Component {
 
+  state = {
+    temp: undefined,
+    text: undefined,
+    city: undefined,
+    state: undefined,
+    country: undefined
+  }
+
   getForecast = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const state = e.target.elements.state.value;
-    const api_call = await fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%2C%20${state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
+    const api_call = await fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20location%2C%20item.condition.temp%2C%20item.condition.text%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${city}%2C%20${state}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
     
     const data = await api_call.json();
 
-    console.log(data);
+    if (city && state) {
+      console.log(data);
+
+      this.setState({
+        temp: data.query.results.channel.item.condition.temp,
+        text: data.query.results.channel.item.condition.text,
+        city: data.query.results.channel.location.city,
+        state: data.query.results.channel.location.region,
+        country: data.query.results.channel.location.country,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temp: undefined,
+        text: undefined,
+        city: undefined,
+        state: undefined,
+        country: undefined,
+        error: "You forgot something."
+      })      
+    }
   }
 
   render() {
@@ -26,7 +54,14 @@ class App extends Component {
       <div>
         <Titles />
         <Form getForecast={this.getForecast} />
-        <Forecast />
+        <Forecast 
+          temp={this.state.temp}
+          text={this.state.text}
+          city={this.state.city}
+          state={this.state.state}
+          country={this.state.country}
+          error={this.state.error}
+        />
       </div>
       
     );
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -4,7 +4,9 @@ class Forecast extends React.Component {
     render() {
         return (
             <div>
-                <p>Looks like rain.</p>
+                { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.state} {this.props.country}</p> }
+                { this.props.temp && <p>Temperature: {this.props.temp}</p> }
+                { this.props.error && <p>{ this.props.error }</p>}
             </div>
         );
     }
