382f2885d6e2e5422ac49ad2c81866f36ce29d80
Jeph Kryzak <jkryzak@credera.com>
2018-05-07 19:29:03 -0500

    refactor form component to include functionality, syntax spacing, updated validation, clear input on form submission

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
-import Forecast from './components/Forecast';
 
 class App extends Component {
-
-  state = {
-    temp: undefined,
-    text: undefined,
-    low: undefined,
-    high: undefined,
-    city: undefined,
-    state: undefined,
-    country: undefined
-  }
-
-  getForecast = async (e) => {
-    e.preventDefault();
-    const location = e.target.elements.location.value;
-    const api_call = await fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20location%2C%20item.condition%2C%20weather.forecast%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${location}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`);
-    
-    const data = await api_call.json();
-
-    if (location) { //if a location is returned...
-      console.log(data); // toss all the info in the console...
-
-      this.setState({ // change the state to this...
-        temp: data.query.results.channel.item.condition.temp,
-        text: data.query.results.channel.item.condition.text,
-        code: data.query.results.channel.item.condition.code,
-        low: data.query.results.channel.item.forecast[0].low,
-        high: data.query.results.channel.item.forecast[0].high,
-        city: data.query.results.channel.location.city,
-        state: data.query.results.channel.location.region,
-        country: data.query.results.channel.location.country,
-        error: ""
-      });
-    } else {
-      this.setState({
-        error: "You forgot something."
-      })      
-    }
-  }
 
   render() {
     return (
       <div className="container-fluid">
         <Title />
-        <Form getForecast={this.getForecast} />
-        <Forecast 
-          temp={this.state.temp}
-          text={this.state.text}
-          code={this.state.code}
-          low={this.state.low}
-          high={this.state.high}
-          city={this.state.city}
-          state={this.state.state}
-          error={this.state.error}
-        />
+        <Form />
       </div>
-      
     );
   }
 }
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 const Forecast = props => (
-    <div>
-        { props.city && 
-            <div>
-                <h3>{props.city}, {props.state}</h3>
-                <p>Currently: <strong>{props.temp}°</strong> and {props.text}</p>
-                <p>
-                    Low: {props.low}°<br/>
-                    High: {props.high}
-                </p>
-            </div>
-        }
-        { props.error && <p>{ props.error }</p>}
-    </div>
+  <div className="forecast">
+    {props.city &&
+      <div>
+        <h3>{props.city}, {props.state}</h3>
+        <p>Currently: <strong>{props.temp}°</strong> and {props.text}</p>
+        <p>
+          Low: {props.low}°<br />
+          High: {props.high}°
+        </p>
+      </div>
+    }
+    {props.error && <p>{props.error}</p>}
+  </div>
 )
 
 export default Forecast;
